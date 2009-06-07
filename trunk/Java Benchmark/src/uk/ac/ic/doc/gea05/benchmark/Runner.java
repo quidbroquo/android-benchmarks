ad811d68475b868723c0fdb71f77d07f6232187d
gav.aiken <gav.aiken@22830572-5305-11de-b413-c5e99dc0a8e2>
2009-06-07 10:36:46 +0000

    All implementations use equivalent code and the C compilers use the -O2 flag

--- trunk/Java Benchmark/src/uk/ac/ic/doc/gea05/benchmark/Runner.java
+++ trunk/Java Benchmark/src/uk/ac/ic/doc/gea05/benchmark/Runner.java
@@ -1,19 +1,27 @@
 package uk.ac.ic.doc.gea05.benchmark;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Runner {
 	private static final int maxPixels = 100000;
-	private static final int step = 100;
+	private static final int step = 1000;
+	private static Map<Integer,Long> times;
+	private static Random rand;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		Random rand = new Random();
+		times = new HashMap<Integer,Long>();
+		rand = new Random();
 		int[] unsorted;
 		int i,j,median;
-		long start, finish;
+		long start, finish,duration;
 		for (i = step; i <= maxPixels; i+= step){
 			unsorted = new int[i];
 
@@ -25,9 +33,40 @@ public class Runner {
 			QuickSort.quicksort(unsorted);
 			finish = System.currentTimeMillis();
 			median = unsorted[(unsorted.length-1)/2];
-			System.out.println(String.format("Elements: %d Time: %d Median: %d",i,(finish-start),median));
+			duration = finish - start;
+			System.out.println(String.format("Elements: %d Time: %d Median: %d",i,duration,median));
+			times.put(i, duration);
 		}
-
+		publishResults(times);
+	    System.out.println("Finished publishing results");
+	}
+	// Logging
+	// For the logs
+	private static BufferedWriter out;
+	
+	// csv
+    private static void publishResults(Map<Integer, Long> times) {
+    	StringBuilder builder = new StringBuilder();
+		builder.append("elements, time,\n");    	
+    	for(Integer inputSize : times.keySet()){
+    		builder.append(inputSize.toString() +","+ times.get(inputSize).toString()+",\n");    		
+    	}
+    	writeToLog(builder.toString());
 	}
 
+    private static final String RESULTS_DIRECTORY = "/home/gavin/Work/benchmark/Results/";
+	private static void writeToLog(String msg){    
+    	try {
+    		if(out==null){
+    		    System.out.println("Creating new file");
+    	        File benchmarkLog = new File(RESULTS_DIRECTORY, "Java_Benchmark.csv");
+    	        benchmarkLog.createNewFile();    	        
+    	        FileWriter writer = new FileWriter(benchmarkLog);
+    	        out = new BufferedWriter(writer);    	    	    	
+    		}
+    		out.write(msg+"\n");
+    	} catch (IOException e) {
+    		System.out.println("Could not write file");
+    	} 
+	}
 }
